f6bae0059bdac94a7236aecc7301887b593f1b91
Salema0x <saleminfo50@gmail.com>
2024-01-16 23:01:48 +0100

    Added navbar, Upload section and Footer, as well as controlling panel and a palcehodler for the generated Pictures upon request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,16 @@
-import React, { useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import ImageUpload from "./ImageUpload";
-import ImageCard from "./ImageCard";
 
-const App = () => {
-  const [imageResults, setImageResults] = useState([0]);
+import React from 'react';
+import Navbar from './components/navbar';
+import Upload from './components/Upload';
+import Footer from './container/Footer';
 
-  const handleImageUpload = (imageUrl) => {
-    // Add logic to send the image URL to the backend for processing
-    // and update the imageResults state with the response.
-    // You can use fetch or axios for API requests.
-    // For now, I'll just add a dummy result to demonstrate.
-    setImageResults([{ id: 1, imageUrl }]);
-  };
-
+function App() {
   return (
-    <Container>
-      <Row className="mt-5">
-        <Col>
-          <ImageUpload onUpload={handleImageUpload} />
-        </Col>
-      </Row>
-      <Row className="mt-3">
-        {imageResults.map((result) => (
-          <Col key={result.id} xs={12} md={4} className="mb-4">
-            <ImageCard imageUrl={result.imageUrl} />
-          </Col>
-        ))}
-      </Row>
-    </Container>
-  );
-};
-
+    <div>
+      <Navbar />
+      <Upload />
+      <Footer />
+    </div>
+      )
+}
 export default App;
