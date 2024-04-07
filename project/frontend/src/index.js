7e341e711cfd6cb12f931da2964bbca4e476c7a6
KingUriel <urielwameni@yahoo.com>
2024-04-07 11:50:23 +0200

    <style> add imagesviewer

--- project/frontend/src/index.js
+++ project/frontend/src/index.js
@@ -1,4 +1,6 @@
 import "./style/uploadbox.css";
+import "./style/imagesgrid.css";
+import "./style/imagesviewer.css";
 import React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
