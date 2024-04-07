5f35245d3184b795333e8908e6e725715731211f
KingUriel <urielwameni@yahoo.com>
2024-04-07 11:52:11 +0200

    <component> Add Utils

---
+++ project/frontend/src/components/Utils.js
@@ -0,0 +1,23 @@
+const Utils = {
+    openFullscreen() {
+        var elem = document.documentElement;
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) { /* Safari */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE11 */
+            elem.msRequestFullscreen();
+        }
+    },
+
+    closeFullscreen() {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) { /* Safari */
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { /* IE11 */
+            document.msExitFullscreen();
+        }
+    }
+}
+export default Utils;
