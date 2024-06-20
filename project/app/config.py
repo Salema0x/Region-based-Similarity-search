4909588e13b1ed89fa08016f68cf9ddf6c0cbf69
KingUriel <urielwameni@yahoo.com>
2024-06-20 04:57:58 +0200

    add config: elasticsearch

---
+++ project/app/config.py
@@ -0,0 +1,42 @@
+#import sys
+#sys.path.append('/project/')
+
+import os
+import logging
+
+def get_env(env, default=""):
+    env_val = os.environ.get(env)
+    if env_val:
+        return env_val
+    else:
+        return default
+
+
+class Config:
+    # Web App settings
+    MAX_UPLOAD_SIZE = 5000  # kB
+    ALLOWED_EXT = ["png", "jpg", "jpeg", "gif", "bmp", "tiff", "tif"]
+    IMAGES_URL = get_env("IMAGES_URL", "")
+    THUMBS_URL = get_env("THUMBS_URL", "")
+    LOCAL_IMAGES = get_env("LOCAL_IMAGES", False)
+
+    # Elastic Search settings
+    ES_MAX_RESULTS = 1000
+    ES_URL = get_env("ES_URL", "http://elasticsearch:9200/es-retrieval/_search?size=")
+
+    #todo with open("/project/app/templates/es_query.7.x.json", 'r') as file:
+    with open("../project/app/templates/es_query.7.x.json", 'r') as file:
+        EL_QUERY_TPL = file.read()
+
+    # Image settings
+    IMG_WIDTH = int(get_env("IMG_WIDTH", 300))
+    IMG_HEIGHT = int(get_env("IMG_HEIGHT", 300))
+
+    # Inference
+    TF_SERVING_HOST = get_env("TF_SERVING_HOST", "tf-serving")
+    TF_SERVING_PORT = int(get_env("TF_SERVING_PORT", "8501"))
+    TF_MODELNAME = get_env("TF_MODELNAME", "model")
+
+
+
+logger = logging.Logger("logs")
