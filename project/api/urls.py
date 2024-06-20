33095c19a7eafc4af3e965a0af0f7a4e9463b753
KingUriel <urielwameni@yahoo.com>
2024-06-20 05:06:15 +0200

    add search_image

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import upload, upload_boxes
+from .views import upload, upload_boxes, search_image_all, search_image_box
 
 urlpatterns = [
     path('api/upload/', upload, name='upload'),
     path('api/draw/', upload_boxes, name='api_draw'),
+    path('api/search/', search_image_all, name='search_image_all'),
+    path('api/searchbox/', search_image_box, name='search_image_box'),
 ]
--- project/api/views.py
+++ project/api/views.py
@@ -1,3 +1,7 @@
+import sys
+
+sys.path.append('/project/')
+
 import requests
 from django.core.files.base import ContentFile
 from rest_framework import status
@@ -8,6 +12,16 @@ from django.core.files.storage import default_storage
 import os
 from django.conf import settings
 
+from app.util import allowed_file, batch_inference, parse_es_results, es_query_str, es_query, es_count, load_image
+from app.config import Config as cfg
+import requests
+import time
+import json
+
+from app.config import logger
+
+
+
 def save_image_to_media(image, subfolder=''):
     """
     Save the given image to the MEDIA_ROOT in the specified subfolder.
@@ -16,6 +30,8 @@ def save_image_to_media(image, subfolder=''):
     file_path = os.path.join(subfolder, image.name)
     saved_image_path_relative = default_storage.save(file_path, ContentFile(image.read()))
     return saved_image_path_relative
+
+
 
 @api_view(['POST'])
 def upload(request):
@@ -84,3 +100,77 @@ def upload_boxes(request):
 
     return Response({"message": "Boxes processed successfully"}, status=status.HTTP_201_CREATED)
 
+
+
+@api_view(['POST'])
+def search_image_all(request):
+    """
+    Perform query and visualize results
+    :return:
+    """
+    images = []
+    error = ""
+    querytime = 0
+    max_results = cfg.ES_MAX_RESULTS
+
+    try:
+        try:
+            img = request.data.get("image")
+            max_results = request.data.get("maxResults")
+        except:
+            error = "Error in form data"
+            raise
+        try:
+            max_results = int(max_results)
+        except:
+            error = "Maximum results is not a number"
+            raise
+        if "image" in request.FILES and request.FILES["image"]:
+            # Query image uploaded
+            file =  request.FILES.get("image")
+            if allowed_file(img.content_type):
+                error = "File type is not accepted"
+                raise Exception(error)
+            else:
+                try:
+                    img = file.read()
+                except:
+                    error = "Could not read file"
+                    raise
+
+        start = time.time()
+        try:
+            img = load_image(img)
+            files = {"query_image": {"qi.jpg": img}}
+            code_dict = batch_inference(files)[0]['codes']
+            q = es_query_str(code_dict)
+
+            res = es_query(q, max_results)
+        except:
+            error = "No result from Elasticsearch"
+            raise
+        try:
+            images = parse_es_results(res)
+        except:
+            error = "Could not parse Elasticsearch results"
+            raise
+
+        querytime = time.time() - start
+
+    except Exception as e:
+        logger.error(e, exc_info=True)
+
+    #num_db_images = es_count()
+    
+    return Response({'duration':querytime,
+                    'images':images,
+                    'thumbs_url':cfg.THUMBS_URL, 
+                    'images_url':cfg.IMAGES_URL}, 
+                    status=status.HTTP_201_CREATED)
+
+
+
+@api_view(['POST'])
+def search_image_box(request):
+    #TODO extract the image from the box
+    return False
